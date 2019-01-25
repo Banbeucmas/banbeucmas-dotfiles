51fc18d68768d0358e514aeba9e0fc9d854d12da
Banbeucmas <banbeucams@gmail.com>
2019-01-25 14:00:57 +0700

    Cleaning

--- .config/polybar/scripts/updates_arch.sh
+++ .config/polybar/scripts/updates_arch.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
-    updates=0
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+    updates_arch=0
 fi
+
+if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
+# if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+    updates_aur=0
+fi
+
+updates=$(("$updates_arch" + "$updates_aur"))
 
 if [ "$updates" -gt 0 ]; then
     echo " $updates "
